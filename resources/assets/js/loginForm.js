13c40abdbfbe1d51df61d4658f42bae13ba10469
Joel <joel@Joels-MacBook-Pro.local>
2015-09-13 16:45:47 -0500

    cleaning up loginForm.js

--- resources/assets/js/loginForm.js
+++ resources/assets/js/loginForm.js
@@ -1,35 +1,24 @@
 $('document').ready(function(){
 
 	$('#login_form').submit(function(){
-
-
-
 		//This will be for the ppl to see
-		//$('#fade_wrapper').fadeIn("53000");
-
+		$('#fade_wrapper').fadeIn("53000");
 
         //Login Functionality
-
         var username = $("input[name='username']").val();
         var password = $("input[name='password']").val();
-
 
 		$.post('/_php/login.php', {username:username, password:password}, function(output){
 
             var json = JSON.parse(output);
 
-
-
             if(json.result){
-                window.location = '/dashboard.php';
+                window.location = '/admin/';
             }else{
                 alert('an error occurred');
             }
 		});
 
-
-
 		return false;
 	})
-
 });
